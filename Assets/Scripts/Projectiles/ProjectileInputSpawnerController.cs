b2f0cd1f412fc975220713e0cad84fe42d1aa996
Sven <steve.vernyi@gmail.com>
2025-07-06 16:21:16 -0500

    More simplification

--- Assets/Scripts/Projectiles/ProjectileInputSpawnerController.cs
+++ Assets/Scripts/Projectiles/ProjectileInputSpawnerController.cs
@@ -1,16 +1,114 @@
 using UnityEngine;
 
-public class ProjectileInputSpawnerController : MonoBehaviour
+public class ProjectileInputSpawnerController : MonoBehaviour, IInteractable
 {
+    [field: SerializeField]
+    public LineRenderer BorderIndicator { get; private set; }
+
+    [field: SerializeField]
+    public CircleCollider2D Collider { get; private set; }
+
+    [field: SerializeField]
+    public int BorderIndicatorSegments { get; private set; }
+
+    [field: SerializeField]
+    public float RotationSpeed { get; private set; }
+
+    [field: SerializeField]
+    public float ForceMultiplier { get; private set; } = 50f;
+
     [field: SerializeField]
     public ProjectileSpawnerController ProjectileSpawner { get; private set; }
 
     [field: SerializeField]
+    public DragIndicatorController DragIndicator { get; private set; }
+
     public PlayerController Player { get; private set; }
 
-    [field: SerializeField]
-    public DragIndicatorController DragIndicator { get; private set; }
+    private bool BorderIndicatorRotating { get; set; }
+
+    private float InitialColliderRadius { get; set; }
+
+    private void Awake()
+    {
+        InitialColliderRadius = Collider.radius;
+
+        InitializeBorderIndicator();
+    }
+
+    private void Start()
+        => Player = PlayerManager.Instance.Player;
+
+    private void Update()
+    {
+        if (!BorderIndicatorRotating)
+            return;
+
+        var offset = Time.time * RotationSpeed;
+        BorderIndicator.material.mainTextureOffset = new Vector2(offset, 0);
+    }
+
+    public void OnDragStart(Vector2 pos)
+    {
+        DragIndicator.StartDrag(pos);
+
+        Collider.radius = 100f;
+    }
+
+    public void OnDragEnd(Vector2 pos)
+    {
+        Collider.radius = InitialColliderRadius;
+
+        var launchForce = DragIndicator.GetLaunchVector() * ForceMultiplier;
+        var launchPosition = DragIndicator.GetLaunchPosition();
+
+        DragIndicator.EndDrag();
+
+        var spawnPosition = launchPosition.ClosestPointInCircle(transform.position, Collider.radius);
+        ProjectileSpawner.SpawnProjectile(PlayerManager.Instance.SelectedProjectileData, spawnPosition, launchForce);
+    }
+
+    public void OnDrag(Vector2 pos)
+        => DragIndicator.Drag(pos);
+
+    public void OnPointerEnter(Vector2 pos)
+        => EnableSpawner();
+
+    public void OnPointerExit(Vector2 pos)
+    {
+        if (!DragIndicator.IsDragging)
+            DisableSpawner();
+    }
 
     public void SpawnProjectile(Vector2 spawnPosition, Vector2 direction)
         => ProjectileSpawner.SpawnProjectile(PlayerManager.Instance.SelectedProjectileData, spawnPosition, direction);
+
+    private void EnableSpawner()
+    {
+        BorderIndicatorRotating = true;
+
+        BorderIndicator.material.color = Color.white;
+    }
+
+    private void DisableSpawner()
+    {
+        BorderIndicatorRotating = false;
+        DragIndicator.EndDrag();
+
+        BorderIndicator.material.color = Color.gray;
+    }
+
+    private void InitializeBorderIndicator()
+    {
+        BorderIndicator.material.color = Color.gray;
+        BorderIndicator.positionCount = BorderIndicatorSegments;
+
+        for (var i = 0; i < BorderIndicatorSegments; i++)
+        {
+            var angle = i * Mathf.PI * 2f / BorderIndicatorSegments;
+            var x = Mathf.Cos(angle) * Collider.radius;
+            var y = Mathf.Sin(angle) * Collider.radius;
+            BorderIndicator.SetPosition(i, new Vector3(x, y, 0));
+        }
+    }
 }
